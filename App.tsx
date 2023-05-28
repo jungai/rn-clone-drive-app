bb5355308b2d9be4f6c3415f523e144b85f15043
jungai <juworaphol125@gmail.com>
2023-05-28 18:21:51 +0700

    feat: init proj with splash screen

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+	return (
+		<View className="flex flex-1 bg-[#25292e] justify-center items-center">
+			<Text className="text-white">Hello</Text>
+		</View>
+	);
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	content: ["./App.{js,jsx,ts,tsx}", "./components/**/*.{jsx,ts,tsx}"],
+	theme: {
+		color: {
+			primary: "#F0C414",
+			secondary: "#000000",
+			base: "#D9D9D9",
+		},
+	},
+	plugins: [],
+};
